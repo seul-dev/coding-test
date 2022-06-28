0b7398b45cd25f4c5f49327e7acd3ae9b4cc2ea6
seul <seul.dev@gmail.com>
2022-06-29 08:23:09 +0900

    완주하지 못한 선수: 해시를 이용한 풀이

--- programmers /lv1/완주하지 못한 선수.js
+++ programmers /lv1/완주하지 못한 선수.js
@@ -1,12 +1,16 @@
 //해시
 //https://programmers.co.kr/learn/courses/30/lessons/42576
 function solution(participant, completion) {
-  participant.sort();
-  completion.sort();
-
-  for (let i = 0; i < participant.length; i++) {
-    if (participant[i] !== completion[i]) {
-      return participant[i];
+  let hashTable = {};
+  for (let p of participant) {
+    !hashTable[p] ? (hashTable[p] = 1) : (hashTable[p] = hashTable[p] + 1);
+  }
+  for (let c of completion) {
+    hashTable[c] ? (hashTable[c] -= 1) : hashTable[c];
+  }
+  for (let p of participant) {
+    if (hashTable[p] >= 1) {
+      return p;
     }
   }
 }
