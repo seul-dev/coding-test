bd99b0a87a0ecc93bbb02873909ccbee2203d67f
seul-dev <seul.dev@gmail.com>
2022-07-08 22:55:34 +0900

    programmers 소수 만들기

---
+++ programmers /lv1/소수 만들기.js
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12977
+function isPrime(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        let sum = nums[i] + nums[j] + nums[k];
+        if (isPrime(sum)) count += 1;
+      }
+    }
+  }
+  return count;
+}
