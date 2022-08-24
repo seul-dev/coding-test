8397d548abcebc10dc03a9fa0467970d6b67b6a0
Seul Lee <seul.dev@gmail.com>
2022-08-24 23:24:02 +0900

    해결 완료

---
+++ programmers /lv2/행렬의 곱셈.js
@@ -0,0 +1,16 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12949
+function solution(arr1, arr2) {
+  const answer = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let row = [];
+    for (let j = 0; j < arr2[0].length; j++) {
+      let sum = 0;
+      for (let k = 0; k < arr2.length; k++) {
+        sum += arr1[i][k] * arr2[k][j];
+      }
+      row.push(sum);
+    }
+    answer.push(row);
+  }
+  return answer;
+}
