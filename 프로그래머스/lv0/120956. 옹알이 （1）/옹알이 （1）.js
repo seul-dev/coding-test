3b018e0f923d9f30f59ba7e79113638c93d87efc
Seul Lee <seul.dev@gmail.com>
2022-12-26 21:03:21 +0900

    [level 0] Title: 옹알이 (1), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,9 @@
+function solution(babbling) {
+  let answer = 0;
+  const regex = /^(aya|ye|woo|ma)+$/;
+  babbling.forEach(word => {
+    if (regex.test(word)) answer++;  
+  })
+
+  return answer;
+}
