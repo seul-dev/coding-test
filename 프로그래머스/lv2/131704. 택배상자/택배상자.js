79567fac02905344bc1c1a2861d213ef21860828
seul-dev <seul.dev@gmail.com>
2023-07-24 21:30:40 +0900

    [level 2] Title: 택배상자, Time: 135.24 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.js
@@ -0,0 +1,16 @@
+function solution(order) {
+    let answer = 0
+    const stack = []
+    
+    for(let i = 1; i<= order.length; i++) {
+        stack.push(i)
+        
+        while(stack.length !== 0 && stack.at(-1) === order[answer]){
+            stack.pop()
+            answer += 1
+        }
+    }
+    
+    return answer
+}
+
