325c2d0233d55049b8fba387dc94e0e42c84703b
seul-dev <seul.dev@gmail.com>
2023-08-04 23:08:07 +0900

    [level 2] Title: 소수 찾기, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,35 @@
+function solution(numbers) {
+    return [...getPermutations(numbers)].filter((num) => isPrimeNumber(num)).length
+}
+
+function getPermutations(str) {
+    const answer = new Set()
+    const check = Array.from({length: str.length}, ()=> 0)
+    
+    const dfs = (cur) => {
+        answer.add(Number(cur))
+        for (let i = 0; i < str.length ; i++ ){
+            if(check[i] === 0) {
+                check[i] = 1
+                dfs(cur + str[i])
+                check[i] = 0
+            }
+        }
+    }
+    
+    dfs('')
+    return [...answer] 
+          
+}
+
+function isPrimeNumber(num) {
+    if(num <= 1) return false; 
+    
+    for(let i = 2; i <= Math.sqrt(num) ; i++) {
+        if(num % i === 0) {
+            return false
+        }
+    }
+    
+    return true
+}
