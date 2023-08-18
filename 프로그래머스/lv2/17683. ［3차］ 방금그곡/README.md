468efcf85cb5bb01c71a8337dcf45fe524d11406
seul-dev <seul.dev@gmail.com>
2023-08-18 19:08:31 +0900

    [level 2] Title: [3차] 방금그곡, Time: 3.74 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/README.md
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 방금그곡 - 17683 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17683) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17683#) 
 
 ### 성능 요약
 
-메모리: 34.5 MB, 시간: 3.96 ms
+메모리: 34.6 MB, 시간: 3.74 ms
 
 ### 구분
 
--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -1,23 +1,16 @@
-function solution(m, musicinfos) {
-    const replaceMusic = (melody) => melody.replaceAll(/(C|D|F|G|A)#/g, (_,letter) => letter.toLowerCase());
-    
-    const getFullMusic = (startTime, endTime, music) => {
-        const [startMin, endMin]=[startTime, endTime].map((v)=> v.split(':').reduce((h, m)=> h * 60 + m,0))
-        const playTime = endMin - startMin
-        const repeatingTime = Math.ceil(playTime / music.length)
-        return [playTime, music.repeat(repeatingTime)]
-    } 
-    
-    const melody = replaceMusic(m)
-    
-    const arr = musicinfos.map((musicInfo)=> {
-        const [start, end, title, music]= musicInfo.split(',')
-        const [playTime, fullMusic] = getFullMusic(start, end, replaceMusic(music))
-        
-        return {title, playTime, fullMusic}
-    }).filter(({title, playTime, fullMusic}) => fullMusic.includes(melody))
-    .sort((a,b)=> b.playTime - a.playTime)
-    
-    return arr[0].title || "(None)"
-}
 
+function solution(m, musicinfos) {
+    m = m.replace(/(C|D|F|G|A)#/g, (_, a) => a.toLowerCase());
+    musicinfos = musicinfos
+        .map((music) => {
+            let [start, end, title, melody] = music.split(',');
+            let h = end.slice(0, 2) - start.slice(0, 2);
+            let m = end.slice(3) - start.slice(3);
+            const time = h * 60 + m;
+            melody = melody.replace(/(C|D|F|G|A)#/g, (_, a) => a.toLowerCase());
+            return {time, title, fullMusic: melody.padEnd(time, melody).slice(0, time)};
+        })
+        .filter(({fullMusic}) => fullMusic.indexOf(m) !== -1)
+        .sort((a, b) => b.time - a.time);
+    return musicinfos[0]?.title || '(None)';
+}
